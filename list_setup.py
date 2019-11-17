871661fdad583a8bbad33fc845d6170de54f4a3f
cglenz <40736491+cglenz@users.noreply.github.com>
2019-11-17 13:07:37 +0400

    Create list_setup.py

---
+++ list_setup.py
@@ -0,0 +1,17 @@
+# import the randint function from the random module
+from random import randint
+
+# create a function to generate a list of "size" random integers
+# up to a maximum of "largest"
+
+def random_list(largest,size):
+  # create an empty list
+  l = []
+  # add a random number to the list the appropriate number of times
+  for i in range(size):
+    n = randint(0,largest-1)
+    l.append(n)
+  #print the list to check
+  print(l)
+#call the function
+random_list(10,10)
